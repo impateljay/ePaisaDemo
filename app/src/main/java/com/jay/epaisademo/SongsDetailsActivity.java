7fd49f4c2e5fa2e423530e87944fc24c4f9e329a
JPatel <pateljay0323@gmail.com>
2016-06-02 19:55:34 +0530

    Changed Track Time

---
+++ app/src/main/java/com/jay/epaisademo/SongsDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.jay.epaisademo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SongsDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_songs_details);
+    }
+}
